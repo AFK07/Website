b93d13cfabc0f7a4df55fea6620f44abc118a240
Gananayak Gurung <97810245+AFK07@users.noreply.github.com>
2024-11-29 14:45:07 +0000

    adding the form and map

--- Everest Peak Cleaning/src/App.js
+++ Everest Peak Cleaning/src/App.js
@@ -7,10 +7,9 @@ import residentCleaning from './images/residentcleaning.png';
 import eventPartyCleanup from './images/event&partycleanup.png';
 import servicedAccom from './images/servicedaccom&airbnb.png';
 
-
 function App() {
   return (
-    <div className="App">
+    <>
       {/* Setting the favicon */}
       <link rel="icon" href={tabLogo} />
 
@@ -61,15 +60,90 @@ function App() {
           </div>
         </div>
       </header>
-      <div class="cleaning-services">
-        <h3>Our Comprehensive Cleaning Services</h3>
-        <div class= "cleaning-details">
-          <img src={residentCleaning} alt="Residential Cleaning" />
-          <img src={eventPartyCleanup} alt="Everest and Party Cleanup" />
-          <img src={servicedAccom} alt="Serviced Accommodation & Airbnb" />
+
+      {/* Cleaning Services Section */}
+      <div className="cleaning-services">
+      <h3>Our Comprehensive Cleaning Services</h3>
+        <div className="cleaning-details">
+          <div className="image-container">
+            <img src={residentCleaning} alt="Residential Cleaning" />
+            <div className="image-text">Residential Cleaning</div>
+          </div>
+          <div className="image-container">
+            <img src={eventPartyCleanup} alt="Event and Party Cleanup" />
+            <div className="image-text">Event and Party Cleanup</div>
+          </div>
+          <div className="image-container">
+            <img src={servicedAccom} alt="Serviced Accommodation & Airbnb" />
+            <div className="image-text">Serviced Accommodation & Airbnb</div>
+          </div>  
+        </div>
+        <div className="view-all-services">
+            <a href="#" className="view-all-button">View All Services </a>
+          </div>
+      </div>
+      
+
+
+      <div class="contact">
+        <div class="contact-info">
+          <h3>Get in Touch for Superior Cleaning Services</h3>
+          <p>Reach out to us by filling out the form or give us a call to schedule a complimentary on-site assessment for your cleaning needs. Service areas include a 20-mile radius from our base in Farnborough.</p>
+          <p>*Please let us know if your cleaning needs fall further than the 20-mile radius.</p>
+          <p>
+            <a href="https://www.google.com/maps/place/Ferneberga+House,+Alexandra+Rd,+Farnborough+GU14+6DQ/@51.2799156,-0.7522033,215m/data=!3m2!1e3!4b1!4m6!3m5!1s0x48742b4ac2f00abd:0x8fd89f90881f6283!8m2!3d51.2799156!4d-0.7515596!16s%2Fg%2F1jkxrj_62?entry=ttu" target="_blank">
+              Suite 1.17 Ferneberga House, Alexander Road, Farnborough, GU14 6DQ
+            </a>
+          </p>
+          <p>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info@everestpeakcleaning.com" target="_blank">
+              info@everestpeakcleaning.com
+            </a>
+          </p>
+          <p><a href="tel:07450403112">07450 403 112</a></p>
+        </div>
+
+        <div class="contact-form">
+          <form class="ContactForm">
+            <div class="client-name">
+              <input type="text" placeholder="First Name" required />
+              <input type="text" placeholder="Last Name" required />
+            </div>
+            <div class="client-contacts">
+              <input type="email" placeholder="Email" required />
+              <input type="tel" placeholder="Phone Number" required />
+            </div>
+            <div class="client-address">
+              <input type="text" placeholder="Address" required />
+            </div>
+            <div class="client-subject">
+              <input type="text" placeholder="Subject" required />
+            </div>
+            <div class="client-enquiry">
+              <textarea placeholder="Type your message here" rows="5" required></textarea>
+            </div>
+            <div class="form-row">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
+      <div
+        className="location"
+        style={{ width: "100%", marginTop: "20px", height: "400px" }}
+      >
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2491.7707151201015!2d-0.7522033!3d51.2799156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48742b4ac2f00abd%3A0x8fd89f90881f6283!2sFerneberga%20House%2C%20Alexandra%20Rd%2C%20Farnborough%20GU14%206DQ!5e0!3m2!1sen!2suk!4v1698695412209!5m2!1sen!2suk"
+          width="100%"
+          height="100%"
+          style={{ border: "none" }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+
+    </>
   );
 }
 
